d9799df913ed3520034dfbf9972102fcbc6478f0
phm0423 <qkrgusalsok@gmail.com>
2025-06-04 17:26:54 +0900

    9일차 커밋

---
+++ day09/Day09Study/MyPortfolioWebApp/Models/AboutModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MyPortfolioWebApp.Models
+{
+    public class AboutModel
+    {
+        public About About { get; set; }
+        public Skill Skill { get; set; }
+    }
+}
---
+++ day09/Day09Study/MyPortfolioWebApp/Models/Skill.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyPortfolioWebApp.Models
+{
+    public class Skill
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Language { get; set; }
+
+        public float Level { get; set; }
+
+        public static implicit operator Skill(List<Skill> v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
